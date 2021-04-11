c6af0123a26e8d4fb9f960bf9c9f7d4e5539bd28
Aitodev <guruitcompany@gmail.com>
2021-04-11 18:49:25 +0600

    vsdv

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,10 +1,18 @@
+const card111 = document.querySelector("#active111");
 const card1 = document.querySelector("#active1");
 const card2 = document.querySelector("#active2");
 const card3 = document.querySelector("#active3");
 
+const text111 = document.querySelector("#text111");
 const text1 = document.querySelector("#text1");
 const text2 = document.querySelector("#text2");
 const text3 = document.querySelector("#text3");
+
+function oneTxt() {
+	text111.classList.toggle("display-block");
+}
+
+card111.addEventListener('click', oneTxt);
 
 function firstTxt() {
 	text1.classList.toggle("display-block");
