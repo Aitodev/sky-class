b54bf7c11ded5530aaf5b7a6e21601278e3c6cce
Aitodev <guruitcompany@gmail.com>
2021-04-12 18:44:14 +0600

    sdvsdv

--- js/accordeon.js
+++ js/accordeon.js
@@ -1,18 +1,24 @@
-const card111 = document.querySelector("#active111");
 const card1 = document.querySelector("#active1");
 const card2 = document.querySelector("#active2");
 const card3 = document.querySelector("#active3");
+const card4 = document.querySelector("#active4");
+const card5 = document.querySelector("#active5");
+const card6 = document.querySelector("#active6");
+const card7 = document.querySelector("#active7");
+const card8 = document.querySelector("#active8");
 
-const text111 = document.querySelector("#text111");
+
 const text1 = document.querySelector("#text1");
 const text2 = document.querySelector("#text2");
 const text3 = document.querySelector("#text3");
+const text4 = document.querySelector("#text4");
+const text5 = document.querySelector("#text5");
+const text6 = document.querySelector("#text6");
+const text7 = document.querySelector("#text7");
+const text8 = document.querySelector("#text8");
 
-function oneTxt() {
-	text111.classList.toggle("display-block");
-}
 
-card111.addEventListener('click', oneTxt);
+
 
 function firstTxt() {
 	text1.classList.toggle("display-block");
@@ -31,3 +37,36 @@ function thirdTxt() {
 }
 
 card3.addEventListener('click', thirdTxt);
+
+
+function ttTxt() {
+	text4.classList.toggle("display-block");
+}
+
+card4.addEventListener('click', ttTxt);
+
+
+function fiveTxt() {
+	text5.classList.toggle("display-block");
+}
+
+card5.addEventListener('click', fiveTxt);
+
+function sixTxt() {
+	text6.classList.toggle("display-block");
+}
+
+card6.addEventListener('click', sixTxt);
+
+function sevenTxt() {
+	text7.classList.toggle("display-block");
+}
+
+card7.addEventListener('click', sevenTxt);
+
+
+function nightTxt() {
+	text8.classList.toggle("display-block");
+}
+
+card8.addEventListener('click', nightTxt);
